c84b22670fad0d0de32da441f037435399ef0cd8
Lutaev D.0 <daniillutaev1@gmail.com>
2023-03-21 12:47:49 +0400

    Добавил парсинг рубля

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 from prettytable import PrettyTable
 from time import sleep
+import xml.etree.ElementTree as ET
 
+usd_rub = float(ET.fromstring(requests.get('https://www.cbr.ru/scripts/XML_daily.asp').text).find('./Valute[CharCode="USD"]/Value').text.replace(',', '.'))
 
 mytable = PrettyTable()
 mytable.field_names = ["Name", "Cost"]
 
-for page in range(1,100):
+for page in range(1,10):
     
     sleep(3)
     url = f"https://steamcommunity.com/market/search?appid=730#p{page}_popular_desc"
@@ -21,6 +23,6 @@ for page in range(1,100):
 
         price = (i.find("span", class_="market_table_value normal_price")).find("span", class_="normal_price").text #Получение цены товара
         cost = float((price.split(' ')[0]).split('$')[1])
-        mytable.add_row([name, round((cost*77.12), 2)+1.0])
+        mytable.add_row([name, round((cost*usd_rub), 2)])
 
 print(mytable)
